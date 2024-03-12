92e3cb0a8dc4a0696155e757c02adc41dec11b23
Azara <yyushahu02@gmail.com>
2024-03-12 22:50:37 +0000

    inheritance init

---
+++ inheritance/0-inheritance.js
@@ -0,0 +1,40 @@
+class Person {
+    name;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    introduceSelf() {
+        console.log(`Hi! I'm ${this.name}`);
+    }
+}
+
+class Professor extends Person {
+    teaches;
+    /*
+    extends is used to depict inheritance from another class
+    the Professor class inherits from the Person class in this case
+    */
+
+    constructor(name, teaches) {
+        super(name);
+        this.teaches = teaches;
+    }
+
+    introduceSelf() {
+        console.log(
+            `My name is ${this.name}, and I will be your ${this.teaches} professor.`,
+        );
+    }
+
+    grade(paper) {
+        const grade = Math.floor(Math.random() * (5 - 1) + 1);
+        console.log(grade);
+    }
+}
+
+const walsh = new Professor('Walsh', 'Psychology');
+walsh.introduceSelf();
+
+walsh.grade('my paper');
